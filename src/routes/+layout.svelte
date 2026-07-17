6cef7690e33468f2d614023ef7a6f7ddeda3c25a
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2026-07-17 07:40:47 +0200

    Add favicons and page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,17 @@
 </script>
 
 <svelte:head>
-    <link rel="icon" href={favicon}/>
+    <title>Friday at Work</title>
+    <link rel="icon" href="/icon16.png" type="image/png" sizes="16x16"/>
+    <link rel="icon" href="/icon64.png" type="image/png" sizes="64x64"/>
+    <link rel="icon" href="/icon128.png" type="image/png" sizes="128x128"/>
+    <link rel="icon" href="/icon256.png" type="image/png" sizes="256x256"/>
+    <link rel="icon" href="/icon512.png" type="image/png" sizes="512x512"/>
+    <link rel="icon" href="/icon1024.png" type="image/png" sizes="1024x1024"/>
+    <link rel="apple-touch-icon" href="/icon167.png" type="image/png" sizes="167x167" />
+    <link rel="apple-touch-icon" href="/icon180.png" type="image/png" sizes="180x180" />
+    <link rel="apple-touch-icon" href="/icon152.png" type="image/png" sizes="152x152" />
+    <link rel="apple-touch-icon" href="/icon120.png" type="image/png" sizes="120x120" />
 </svelte:head>
 
 {#if data.isLoggedIn && data.hasWorkspace }
