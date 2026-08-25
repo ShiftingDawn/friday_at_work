9008c327ca90b43ecfd2e7c847238f8233caaaf6
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2026-08-25 19:15:07 +0200

    Fix card using wrong header color

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -1,41 +1,41 @@
 <!-- eslint-disable @typescript-eslint/no-explicit-any  -->
 <!-- eslint-disable-next-line @stylistic/quotes -->
 <script lang="ts" generics="T extends import('svelte').Component<any> | keyof HTMLElementTagNameMap = 'div'">
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  import type {Component, ComponentProps, Snippet} from "svelte";
-  import {twMerge} from "tailwind-merge";
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    import type {Component, ComponentProps, Snippet} from "svelte";
+    import {twMerge} from "tailwind-merge";
 
-  type Props = (
-    T extends keyof HTMLElementTagNameMap
-      ? svelteHTML.IntrinsicElements[T]
-      : T extends Component<any>
-        ? ComponentProps<T>
-        : Record<string, any>
-    ) & {
-      as?: T;
-      back?: Snippet;
-      action?: Snippet;
-      children?: Snippet;
-    };
+    type Props = (
+        T extends keyof HTMLElementTagNameMap
+          ? svelteHTML.IntrinsicElements[T]
+          : T extends Component<any>
+            ? ComponentProps<T>
+            : Record<string, any>
+        ) & {
+          as?: T;
+          back?: Snippet;
+          action?: Snippet;
+          children?: Snippet;
+        };
 
-  let {as = "div" as any, title, back, action, class: cls, children, ...rest}: Props = $props();
+    let {as = "div" as any, title, back, action, class: cls, children, ...rest}: Props = $props();
 </script>
 
 <svelte:element this={as} class={twMerge("bg-surface0 p-4 rounded-2xl shadow-lg", cls)} {...rest}>
-    <div class="flex flex-col gap-2">
-        {#if back || title || action}
-            <div class="flex items-center justify-between h-16 mb-2 bg-secondary p-4 rounded-lg">
-                <div class="flex items-center gap-2">
-                    {@render back?.()}
-                    {#if title}
-                        <span class="font-bold text-xl uppercase">{title}</span>
-                    {/if}
-                </div>
-                <div class="flex items-center gap-2">
-                    {@render action?.()}
-                </div>
-            </div>
-        {/if}
-        {@render children?.()}
-    </div>
+  <div class="flex flex-col gap-2">
+    {#if back || title || action}
+      <div class="flex items-center justify-between h-16 mb-2 bg-base-alt p-4 rounded-lg">
+        <div class="flex items-center gap-2">
+          {@render back?.()}
+          {#if title}
+            <span class="font-bold text-xl uppercase">{title}</span>
+          {/if}
+        </div>
+        <div class="flex items-center gap-2">
+          {@render action?.()}
+        </div>
+      </div>
+    {/if}
+    {@render children?.()}
+  </div>
 </svelte:element>
