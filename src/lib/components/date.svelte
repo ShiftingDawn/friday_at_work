b62ff97c1ae697cc31189bbe493ef7803397ab2b
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2026-08-25 15:53:26 +0200

    Maybe fixed date timestamps

--- src/lib/components/date.svelte
+++ src/lib/components/date.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
+  import { showTime } from "$lib/preferences";
+
   const {
     value,
      fallback
   }: {
     value: Date | undefined, fallback?: string
   } = $props();
+
+  const showTimestamp = $showTime;
  
   function displayDate<T extends Date | undefined>(date: T): T extends Date ? string : undefined {
     const str = date?.toLocaleDateString("en-US", {dateStyle: "long",}) as T extends Date ? string : undefined;
-    if (typeof localStorage !== "undefined" && "showtime" in localStorage && date) {
+    if (showTimestamp && date) {
       return `${str} at ${date.toLocaleString("nl-NL", {timeStyle: "short",})}` as T extends Date ? string : undefined;
     }
     return str;
